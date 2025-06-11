6452deca8730992a3b76b2ddff2a26867cab3ffc
alwi <alwiarfansolin@gmail.com>
2025-06-11 15:18:05 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
-import theme from './theme/theme';
+import theme from './theme/Theme';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
